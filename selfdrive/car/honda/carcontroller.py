19882c2e6a0ba0cd193ead2e3ef49d4be35b9eae
nandrews283 <39073836+nandrews283@users.noreply.github.com>
2020-03-18 14:28:24 -0500

    making sure things are being sent

--- selfdrive/car/honda/carcontroller.py
+++ selfdrive/car/honda/carcontroller.py
@@ -175,5 +175,9 @@ class CarController():
           # send exactly zero if apply_gas is zero. Interceptor will send the max between read value and apply_gas.
           # This prevents unexpected pedal range rescaling
           can_sends.append(create_gas_command(self.packer, apply_gas, idx))
+          
+    print("--------------------------------------------------")
+    print(can_sends)
+    print("--------------------------------------------------")
 
     return can_sends
